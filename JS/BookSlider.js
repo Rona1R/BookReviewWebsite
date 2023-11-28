bf85b30f86b272af66298f56c051226e2a17ac0d
Rona1R <rushitirona@gmail.com>
2023-11-28 01:35:53 +0100

    I shtova fotot e librave+disa ngjyra/fonte ne css i ndrrova

--- JS/BookSlider.js
+++
@@ -1,8 +0,0 @@
-function scrollSlider(direction) {
-    const container = document.querySelector('.slider-container');
-    const scrollAmount = direction === 'left' ? -300 : 300;
-    container.scrollBy({
-      left: scrollAmount,
-      behavior: 'smooth'
-    });
-  }
